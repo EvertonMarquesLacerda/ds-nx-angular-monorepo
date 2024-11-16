182dbc25bf652113e36080da108892f86b917697
everton-lacerda <everton_ml@outlook.com>
2024-11-16 20:13:26 -0300

    add lib button

--- apps/ds-nx-angular-monorepo/src/app/app.component.ts
+++ apps/ds-nx-angular-monorepo/src/app/app.component.ts
@@ -1,10 +1,11 @@
+import { ButtonComponent } from './../../../../libs/button/src/lib/button/button.component';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, ButtonComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
