8923b217414550ea8060bb9b82edd9b99f223186
everton-lacerda <everton_ml@outlook.com>
2024-11-18 22:07:51 -0300

    config lib css

--- libs/storybook-host/.storybook/main.ts
+++ libs/storybook-host/.storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from '@storybook/angular';
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.stories.@(ts|js|mdx)'],
+  stories: ['../src/**/*.stories.@(ts|js|mdx)', '../src/lib/**/*.mdx'],
   addons: ['@storybook/addon-essentials'],
   framework: {
     name: '@storybook/angular',
